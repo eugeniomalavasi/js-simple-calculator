846cb113405bef21738907e8bef8ce27c1864ead
eugeniomalavasi <eugenio.malavasi@gmail.com>
2024-04-08 16:38:58 +0200

    feat: milestone 1 done

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 let numberSelector = document.querySelectorAll(".numbers .shadow");
 console.log(numberSelector);
 
+// numero nello schermo nero
+const screenNumb = document.querySelector(".screen_text");
+console.log(screenNumb.innerHTML);
+
+// ciclo for per assegnare event listener ad ogni bottone
 for (let i = 0; i < numberSelector.length; i++) {
     let singleNub = numberSelector[i];
-    singleNub.addEventListener("click", btnClick)
+    singleNub.addEventListener("click", btnClick);
 }
 
+/**
+ * concatena numero input con numero nel display nero
+ * @returns {number}
+ */
 function btnClick() {
-    
-    console.log(this.innerHTML);
+    const inputNumb = this.innerHTML;
+    console.log(inputNumb);
+    const blackScreenResult = `${screenNumb.innerHTML}${inputNumb}`;
+    screenNumb.innerHTML = blackScreenResult;
 }
+
+
